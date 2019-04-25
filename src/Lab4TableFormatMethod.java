ff3a9a91659edb1463b9e3a22049b69e13e308d3
maryeddy <maryeddy0@gmail.com>
2019-04-25 10:10:37 -0400

    update

--- src/Lab4TableFormatMethod.java
+++ src/Lab4TableFormatMethod.java
@@ -1,42 +1,41 @@
 import java.util.Scanner;
+
 public class Lab4TableFormatMethod {
-	
-		public static void main(String[] args) {
-			Scanner scan = new Scanner(System.in);
 
-			String repeat = null;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
-			do {
-				System.out.println("Please enter a number");
-				int num = scan.nextInt();
+		String repeat = null;
 
-				// set left justified
-				System.out.printf("%-10s | %-12s | %-12s|\n", " Numbers", " Squared", " Cubed");
-				System.out.printf("-----------------------------------------------\n");
+		do {
+			System.out.println("Please enter a number");
+			int num = scan.nextInt();
 
-				for (int i = 0; i <= num; i++) {
-					System.out.printf("%6s\n", (i));
-					System.out.printf("%18s\n", square(i));
-					System.out.printf("%33s\n", cubed(i));
-				}
+			// set left justified
+			System.out.printf(" | %-10s | %-12s | %-12s|\n", " Numbers", " Squared", " Cubed ");
+			System.out.printf("-----------------------------------------------\n");
+			String format = " |%-10s | %-12s | %-12s|\n ";
 
-				System.out.println();
-				System.out.println("Would you like to enter another number? (yes/no)");
-				repeat = scan.next();
+			for (int i = 1; i <= num; i++) {
+				System.out.printf(format,(i), square(i), cubed(i));
 
-			} while (repeat.equalsIgnoreCase("yes"));
-			System.out.println("goodbye.");
+			}
 
-		}
+			System.out.println();
+			System.out.println("Would you like to enter another number? (yes/no)");
+			repeat = scan.next();
 
-		public static int square(int num) {
-			return (num * num);
-		}
-
-		public static int cubed(int num) {
-			return ((num * num) * num);
-		}
+		} while (repeat.equalsIgnoreCase("yes"));
+		System.out.println("goodbye.");
 
 	}
 
+	public static int square(int num) {
+		return (num * num);
+	}
 
+	public static int cubed(int num) {
+		return ((num * num) * num);
+	}
+
+}
